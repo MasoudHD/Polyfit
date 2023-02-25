c289510614e5da3f1d322a7e9ede744d08e7177c
Masoud <60979824+MasoudHD@users.noreply.github.com>
2023-02-25 22:50:09 +0330

    20230225-2245 | Remove build folder

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/mheid/OneDrive/Desktop/polyfit
-# Build directory: C:/Users/mheid/OneDrive/Desktop/polyfit/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
